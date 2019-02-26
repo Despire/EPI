884e4a75453657d805759dfeece103b0a0214b62
matusmrekaj <matus.mrekaj@icloud.com>
2019-02-26 18:53:49 +0100

    fixed formatting

--- arrays/dutchflagproblem_test.go
+++ arrays/dutchflagproblem_test.go
@@ -11,11 +11,11 @@ func TestPartition(t *testing.T) {
 		in   [5]int
 		want [5]int
 	}{
-		{0, [...]int{5, 4, 3, 2, 1}, [...]int{4,3,2,1,5}},
-		{1, [...]int{5, 4, 3, 2, 1}, [...]int{1,3,2,4,5}},
-		{2, [...]int{5, 4, 3, 2, 1}, [...]int{1,2,3,4,5}},
-		{3, [...]int{5, 4, 3, 2, 1}, [...]int{1,2,3,4,5}},
-		{1, [...]int{3, 3, 3, 2, 1}, [...]int{2,1,3,3,3}},
+		{0, [...]int{5, 4, 3, 2, 1}, [...]int{4, 3, 2, 1, 5}},
+		{1, [...]int{5, 4, 3, 2, 1}, [...]int{1, 3, 2, 4, 5}},
+		{2, [...]int{5, 4, 3, 2, 1}, [...]int{1, 2, 3, 4, 5}},
+		{3, [...]int{5, 4, 3, 2, 1}, [...]int{1, 2, 3, 4, 5}},
+		{1, [...]int{3, 3, 3, 2, 1}, [...]int{2, 1, 3, 3, 3}},
 	}
 
 	for _, test := range tests {
@@ -28,17 +28,17 @@ func TestPartition(t *testing.T) {
 
 		pivot := test.in[test.i]
 		for i := 0; i < e1; i++ {
-			if ! (got[i] < pivot) {
+			if !(got[i] < pivot) {
 				t.Errorf("elements that are less than the pivot are not correct")
 			}
 		}
 		for i := e1; i < e2; i++ {
-			if ! (got[i] == pivot) {
+			if !(got[i] == pivot) {
 				t.Errorf("elements that are equal to the pivot are not correct")
 			}
 		}
 		for i := e2; i < len(got); i++ {
-			if ! (got[i] > pivot) {
+			if !(got[i] > pivot) {
 				t.Errorf("elements that are greater than the pivot are not correct")
 			}
 		}
