23e6311e635c3a6727c645dd3c9caa4c9d4e68bf
matusmrekaj <matus.mrekaj@icloud.com>
2019-02-27 19:17:11 +0100

    add test case

--- arrays/buyonce_test.go
+++ arrays/buyonce_test.go
@@ -11,6 +11,7 @@ func TestBuyAndSellStockOnce(t *testing.T) {
 		{[]int{210, 315, 275, 295, 260, 270, 290, 230, 255, 250}, 105},
 		{[]int{2, 4, 2, 6, 3, 0, 5}, 5},
 		{[]int{200, 100}, 0},
+		{nil, 0},
 	}
 	for _, test := range tests {
 		got := BuyAndSellStockOnce(test.in)
